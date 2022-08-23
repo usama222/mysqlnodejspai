7731d585af37420ca856a6706d33acedc03d776d
Usama <uk690952@gmail.com>
2022-08-23 14:31:02 +0500

    routes changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,28 +12,28 @@ const UserController = require('../controllers/UserController');
 // router.get('/employees', EmployeeController.Index);
 
 router.get('/roles', RoleController.show);
-router.post('/role/store', RoleController.store);
-router.get('/role/edit/:id', RoleController.edit);
-router.put('/role/update', RoleController.update);
-router.delete('/role/delete/:id', RoleController.delete)
+router.post('/roles/store', RoleController.store);
+router.get('/roles/edit/:id', RoleController.edit);
+router.put('/roles/update', RoleController.update);
+router.delete('/roles/delete/:id', RoleController.delete)
 
 router.get('/customers', CustomerController.show);
-router.post('/customer/store', CustomerController.store);
-router.get('/customer/edit/:id', CustomerController.edit);
-router.put('/customer/update', CustomerController.update);
-router.delete('/customer/delete/:id', CustomerController.delete)
+router.post('/customers/store', CustomerController.store);
+router.get('/customers/edit/:id', CustomerController.edit);
+router.put('/customers/update', CustomerController.update);
+router.delete('/customers/delete/:id', CustomerController.delete)
 
 router.get('/suppliers', SupplierController.show);
-router.post('/supplier/store', SupplierController.store);
-router.get('/supplier/edit/:id', SupplierController.edit);
-router.put('/supplier/update', SupplierController.update);
-router.delete('/supplier/delete/:id', SupplierController.delete)
+router.post('/suppliers/store', SupplierController.store);
+router.get('/suppliers/edit/:id', SupplierController.edit);
+router.put('/suppliers/update', SupplierController.update);
+router.delete('/suppliers/delete/:id', SupplierController.delete)
 
 router.get('/users', UserController.show);
-router.post('/user/store', UserController.store);
-router.get('/user/edit/:id', UserController.edit);
-router.put('/user/update', UserController.update);
-router.delete('/user/delete/:id', UserController.delete)
+router.post('/users/store', UserController.store);
+router.get('/users/edit/:id', UserController.edit);
+router.put('/ususerser/update', UserController.update);
+router.delete('/users/delete/:id', UserController.delete)
 
 
 module.exports = router;
