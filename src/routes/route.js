2be689b782e9e1ed9bf049ed34d34dce4e811446
Usama <uk690952@gmail.com>
2022-08-23 20:08:54 +0500

    update routese

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,9 +13,9 @@ const UserController = require('../controllers/UserController');
 
 router.get('/roles', RoleController.show);
 router.post('/roles/store', RoleController.store);
-router.get('/roles/edit/:id', RoleController.edit);
-router.put('/roles/update', RoleController.update);
-router.delete('/roles/delete/:id', RoleController.delete)
+router.get('/roles/:id', RoleController.edit);
+router.put('/roles/:id', RoleController.update);
+router.delete('/roles/:id', RoleController.delete)
 
 router.get('/customers', CustomerController.show);
 router.post('/customers/store', CustomerController.store);
