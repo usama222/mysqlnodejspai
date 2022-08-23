614722f6487d5928f51cc82990f3f026b02d6571
Usama <uk690952@gmail.com>
2022-08-23 12:28:11 +0500

    add create error package on app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // require("./src/config/db");
 const router = require('./src/routes/route');
-// const createError = require("http-errors");
+const createError = require("http-errors");
 
 const port = process.env.PORT || 5000;
 
