199b490ecf8bb23162a84f16b47302d37877aae5
Usama <uk690952@gmail.com>
2022-09-03 17:34:17 +0500

    change database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql');
 const con = mysql.createConnection({
-    host: 'mydb.c2y3cmgycsoc.us-east-1.rds.amazonaws.com',
-    user: 'admin',
-    password: '12345678',
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bcc42c86ec168f',
+    password: '0890ae4e',
     port: '3306',
-    database: 'mysqlnodejs',
+    database: 'heroku_574bf8d2766e4e0',
 
 });
+// host: 'mydb.c2y3cmgycsoc.us-east-1.rds.amazonaws.com',
+//     user: 'admin',
+//     password: '12345678',
+//     port: '3306',
+//     database: 'mysqlnodejs',
 
-
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected");
+});
 module.exports = con;
