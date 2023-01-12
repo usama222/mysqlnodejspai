0c7cc9ce2b60801ec43bfb3c323bf45142d29988
Usama <uk690952@gmail.com>
2023-01-12 12:37:41 +0500

    change app.js to index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const router = require('./src/routes/route');
 const createError = require("http-errors");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
